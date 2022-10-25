c8d055b3c77eb12e7d7b14fcff802b1c3f2be111
Aman Gupta <guptaaman704@gmail.com>
2022-10-26 00:33:12 +0530

    event models added and requirements.txt updated

---
+++ events/constants.py
@@ -0,0 +1,11 @@
+from djchoices import ChoiceItem, DjangoChoices
+
+
+class EventTypes(DjangoChoices):
+    individual = ChoiceItem(value="INDIVIDUAL", label=("individual event"))
+    team = ChoiceItem(value="TEAM", label=("team event"))
+
+
+class ClubTypes(DjangoChoices):
+    cultural = ChoiceItem(value="CULTURAL", label=("cultural club"))
+    technical = ChoiceItem(value="TECHNICAL", label=("technical club"))
--- events/models.py
+++ events/models.py
@@ -1,3 +1,45 @@
+import uuid
+from email.policy import default
+
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from events.constants import ClubTypes, EventTypes
+
+
+class Club(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+    type = models.CharField(max_length=15, choices=ClubTypes)
+
+
+class Event(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+    type = models.CharField(
+        max_length=15, choices=EventTypes, default=EventTypes.individual
+    )
+    description = models.TextField()
+    datetime = models.DateTimeField()
+    duration = models.TimeField()
+    venue = models.CharField(max_length=100)
+    club = models.ForeignKey(Club, on_delete=models.CASCADE, related_name="events")
+    max_team_size = models.IntegerField(default=1)
+    min_team_size = models.IntegerField(default=1)
+    image_url = models.URLField()
+
+
+class Team(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=25, null=True, blank=True)
+    event = models.ForeignKey(
+        Event, on_delete=models.CASCADE, related_name="teamsregistered"
+    )
+    is_registered = models.BooleanField(default=False)
+    join_url = models.URLField()
+
+
+class TeamMembers(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="user")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="teams")
+    is_leader = models.BooleanField(default=False)
