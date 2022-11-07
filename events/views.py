0df61caf37aab8e88051bdc434e51a0476adfbf5
Daksh Verma <dakshverma.verma@gmail.com>
2022-11-07 16:43:08 +0530

    fixed status code

--- events/views.py
+++ events/views.py
@@ -98,14 +98,15 @@ class TeamDetailsAPIView(GenericAPIView):
             if str(t.team.event.id) == event_id:
                 if t.team.event.type == "TEAM":
                     response = {"team_name":t.team.name}
-                    response['is_registered'] = t.team.is_registered
+                    response['is_registered'] = True
+                    response['team_valid'] = t.team.is_registered # if min and max size are fulfilled
                     response['members'] = self.get_participants_json_from_team(t.team)
                     response['event_type'] = t.team.event.type
                     return Response(response, status.HTTP_200_OK)
                 else:
-                    response['is_registered'] = t.team.is_registered
+                    response['is_registered'] = True
                     response['members'] = self.get_participants_json_from_team(t.team)
                     response['event_type'] = t.team.event.type
                     return Response(response, status.HTTP_200_OK)
         
-        return Response({"error":"not registered for this event"}, status.HTTP_400_BAD_REQUEST)
+        return Response({"is_registered":False}, status.HTTP_200_OK)
